24428479782f937c78a046f82e7fd1b232e8d1c6
MetaEMK <jan-benecke@t-online.de>
2022-11-30 20:14:55 +0100

    update validationError
    adding errorcodes enum

--- http/validators/validationError.ts
+++ http/validators/validationError.ts
@@ -1,4 +1,4 @@
-class ValidationError 
+export class ValidationError 
 {
     public code: string;
     public message: string;
@@ -7,4 +7,11 @@ class ValidationError
         this.code = code;
         this.message = message;
     }
+}
+
+export enum GenerelValidationErrorCodes {
+    NAME_MISSING = "NAME_MISSING",
+    NAME_INVALID = "INVALID_NAME",
+    NAME_INVALID_LENGTH = "INVALID_NAME_LENGTH",
+    NAME_INVALID_UNIQUE = "INVALID_NAME_UNIQUE",
 }
