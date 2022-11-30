8c7b294e7100bd17a42b4442402f3a45eecece3a
Jonny Vang <jonny.vang@nussbaum-medien.de>
2022-11-30 11:45:21 +0100

    Codebase - Removed unused files and adjusted config files

--- services/echo.ts
+++
@@ -1,9 +0,0 @@
-// import {createEchoLog, Echo, EchoInput, queryEchos} from "../models/echo.js";
-
-// export function saveEcho(echo: EchoInput): Promise<Echo> {
-//     return createEchoLog(echo);
-// }
-
-// export function listEchos(messageContains?: string): Promise<Echo[]> {
-//     return queryEchos(messageContains);
-// }
--- util/logger.ts
+++
@@ -1,4 +0,0 @@
-export function logToConsole(req, res, next): void {
-    console.log(`${req.method} Request on ${req.url}`);
-    next();
-}
