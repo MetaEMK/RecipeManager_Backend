aa2f58cb170aed74a62f8d4f1440ce1b8e746268
MetaEMK <Jan-Benecke@t-online.de>
2022-11-30 15:46:51 +0100

    delete testfile

--- utils/testfiles.ts
+++
@@ -1,9 +0,0 @@
-import * as file from "./files.js";
-
-let test = await file.getAllFilesAsync("test");
-if(!test) console.log("Error");
-else
-for (let index = 0; index < test.length; index++) {
-    console.log(file.getFileInfo("test/" + test[index]));
-    
-}
