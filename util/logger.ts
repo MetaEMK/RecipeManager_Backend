d144784f0bc0d7400edea61daff49aef9debbcea
MetaEMK <Jan-Benecke@t-online.de>
2022-11-30 09:51:07 +0100

    improved logger
    - logger can now log sync
    - improved config for max file count and max file size
    - bugfix in loglevel

--- util/logger.ts
+++ util/logger.ts
@@ -1,92 +1,106 @@
 import { LOG_ENDPOINT, LOG_LEVEL, LOG_LEVEL_STRING } from './logger_enum.js';
-import * as fs from 'fs/promises';
+import * as fsAsync from 'fs/promises';
+import * as fs from 'node:fs';
 import dotenv from 'dotenv';
-import { getFileSize } from './files.js';
 
 class Logger
 {
     private pwd = process.env.PWD!;
-    private setup = false;
+    private logsize: number = 50000000;
+    private logcount: number = 10;
+
+    private setup: boolean = false;
     private logLevel: LOG_LEVEL;
     private logPath: string = './log';
     private archivePath = this.logPath + '/archive';
 
     constructor()
     {
-        dotenv.config({ path: '/config/logging.env' });
-        this.logLevel = process.env.LOG_LEVEL ? parseInt(process.env.LOG_LEVEL!) : LOG_LEVEL.LOG_LEVEL_INFO;
+        console.log("Creating Logger");
+
+        dotenv.config({ path: this.pwd + '/config/logging.env' });
+
+        this.logLevel = process.env.LOG_LEVEL! ? parseInt(process.env.LOG_LEVEL!) : LOG_LEVEL.LOG_LEVEL_INFO;
+        this.logsize = process.env.MAX_LOG_SIZE! ? parseInt(process.env.MAX_LOG_SIZE!) : 50000000;
+        this.logcount = process.env.MAX_LOG_FILES! ? parseInt(process.env.MAX_LOG_FILES!) : 10;
+        this.logPath = process.env.LOG_PATH! ? this.pwd + process.env.LOG_PATH! : this.pwd + '/log';
+        this.archivePath = this.logPath + '/archive';
+
+        console.log("Log Level: " + this.logLevel);
+        console.log("Log Size: " + this.logsize);
+        console.log("Log Count: " + this.logcount);
+        console.log("Log Path: " + this.logPath);
+        console.log("Archive Path: " + this.archivePath);
     }
 
     public async create(): Promise<void>
     {
         if (this.setup) return;
         const endpoints: string[] = Object.values(LOG_ENDPOINT);
-        if (!await doesDirectoryExist(this.logPath)) await fs.mkdir(this.logPath);
+        if (!await doesDirectoryExist(this.logPath)) await fsAsync.mkdir(this.logPath);
         await deleteContent(this.archivePath);
-        if(!await doesDirectoryExist(this.archivePath)) await fs.mkdir(this.archivePath);
+        if(!await doesDirectoryExist(this.archivePath)) await fsAsync.mkdir(this.archivePath);
         await delay(200);
-        let files = await fs.readdir(this.logPath);
+        let files = await fsAsync.readdir(this.logPath);
         files = files.filter(file => !file.includes('archive'));
         for (let index = 0; index < (files).length; index++) {
             
-            await fs.copyFile(this.logPath + '/' + files[index], this.archivePath + '/' + files[index]);
-            await fs.rm(this.logPath + '/' + files[index]);
+            await fsAsync.copyFile(this.logPath + '/' + files[index], this.archivePath + '/' + files[index]);
+            await fsAsync.rm(this.logPath + '/' + files[index]);
         }
         for (const endpoint of endpoints) {
-            await fs.appendFile(this.logPath + endpoint, '');
-            await this.log("Created log file for " + endpoint, LOG_LEVEL.LOG_LEVEL_INFO, LOG_ENDPOINT.LOGGER);
+            await fsAsync.appendFile(this.logPath + endpoint, '');
+            this.log("Created log file for " + endpoint, LOG_LEVEL.LOG_LEVEL_INFO, LOG_ENDPOINT.LOGGER);
         }
         this.setup = true;
     }
 
-    private async log(message: string, level: LOG_LEVEL,endpoint: LOG_ENDPOINT)
+    private log(message: string, level: LOG_LEVEL,endpoint: LOG_ENDPOINT): void
     {
-        await this.archiveLogFile(endpoint);
+        this.archiveLogFile(endpoint);
         let lev = LOG_LEVEL_STRING[level];
-        let mes = `${new Date().toISOString()}\t [${lev}]\t ${message}\n`;
-        try {fs.appendFile(this.logPath + endpoint, mes);}
-        catch {console.log("Error while logging");}
+        let mes = `${new Date().toISOString()}\t [${lev}]\t\t ${message}\n`;
+        try {fs.appendFileSync(this.logPath + endpoint, mes);}
+        catch (error) {console.log("Error while logging");}
     }
 
 
-    private async archiveLogFile(endpoint: LOG_ENDPOINT): Promise<void> 
+    private archiveLogFile(endpoint: LOG_ENDPOINT): void
     {
-        if(!await doesFileExist(this.logPath + endpoint))return;
-        if(await getFileSize(this.logPath + endpoint) > 10000)
+        if(!doesFileExist(this.logPath + endpoint))return;
+        if(getFileSize(this.logPath + endpoint) > this.logsize)
         {
-            await fs.copyFile(this.logPath + endpoint, this.logPath + endpoint + new Date().toISOString());
-            await fs.truncate(this.logPath + endpoint, 0);
-            let files = await fs.readdir(this.logPath);
+            fs.copyFileSync(this.logPath + endpoint, this.logPath + endpoint + new Date().toISOString());
+            fs.truncateSync(this.logPath + endpoint, 0);
+            let files = fs.readdirSync(this.logPath);
             files = files.filter(file => ("/" + file).startsWith(endpoint) == true);
-            if (files.length > 3) {
-;                await fs.rm(this.logPath + '/' + files[1]);
+            if (files.length-1 > this.logcount) {
+;                fs.rmSync(this.logPath + '/' + files[1]);
             }
-            await this.info("Archived log file for " + endpoint, LOG_ENDPOINT.LOGGER);
-
+            this.info("Archived log file for " + endpoint, LOG_ENDPOINT.LOGGER);
         }
     }
 
-    public async debug(message: string, endpoint: LOG_ENDPOINT): Promise<void>
+    public debug(message: string, endpoint: LOG_ENDPOINT): void
     {
-        if (this.logLevel < LOG_LEVEL.LOG_LEVEL_DEBUG) return;
-        await this.log(message, LOG_LEVEL.LOG_LEVEL_DEBUG, endpoint);
+        if (this.logLevel > LOG_LEVEL.LOG_LEVEL_DEBUG) return;
+        this.log(message, LOG_LEVEL.LOG_LEVEL_DEBUG, endpoint);
     }
-    public async info(message: string, endpoint: LOG_ENDPOINT)
+    public info(message: string, endpoint: LOG_ENDPOINT): void
     {
-        if (this.logLevel < LOG_LEVEL.LOG_LEVEL_INFO) {
+        if (this.logLevel > LOG_LEVEL.LOG_LEVEL_INFO) {
             return;
         }
         this.log(message, LOG_LEVEL.LOG_LEVEL_INFO, endpoint);
     }
-    public async warn(message: string, endpoint: LOG_ENDPOINT): Promise<void>
+    public warn(message: string, endpoint: LOG_ENDPOINT): void
     {
         if (this.logLevel > LOG_LEVEL.LOG_LEVEL_WARN) return;
-        await this.log(message, LOG_LEVEL.LOG_LEVEL_WARN, endpoint);
+         this.log(message, LOG_LEVEL.LOG_LEVEL_WARN, endpoint);
     }
-    public async error(message: string, endpoint: LOG_ENDPOINT): Promise<void>
+    public error(message: string, endpoint: LOG_ENDPOINT): void
     {
-        if (this.logLevel > LOG_LEVEL.LOG_LEVEL_ERROR) return;
-        await this.log(message, LOG_LEVEL.LOG_LEVEL_ERROR, endpoint);
+        this.log(message, LOG_LEVEL.LOG_LEVEL_ERROR, endpoint);
     }
 }
 const logger = async () => {
@@ -101,11 +115,11 @@ export async function createLogger(): Promise<Logger>
 
 
 
-async function doesFileExist(path: string): Promise<boolean>
+function doesFileExist(path: string): boolean
 {
     try 
     {
-        return (await fs.stat(path)).isFile();
+        return (fs.statSync(path)).isFile();
     }
     catch
     {
@@ -113,11 +127,11 @@ async function doesFileExist(path: string): Promise<boolean>
     }
 }
 
-async function doesDirectoryExist(path: string): Promise<boolean>
+function doesDirectoryExist(path: string): boolean
 {
     try 
     {
-        return (await fs.stat(path)).isDirectory();
+        return (fs.statSync(path)).isDirectory();
     }
     catch
     {
@@ -128,14 +142,21 @@ async function doesDirectoryExist(path: string): Promise<boolean>
 export async function deleteContent(path: string): Promise<void>
 {
     if(!await doesDirectoryExist(path)) return;
-    const files = await fs.readdir(path);
+    const files = await fsAsync.readdir(path);
     let tasks: Promise<void>[] = [];
     for (const file of files) {
-        tasks.push(fs.rm(path + '/' + file));
+        tasks.push(fsAsync.rm(path + '/' + file));
     }
     await Promise.all(tasks);
 }
 function delay(ms: number)
 {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+export function getFileSize(path: string): number
+{
+    if(!doesFileExist(path)) return 0;
+    let size: number = (fs.statSync(path)).size;
+    //createLogger().then(logger => logger.debug("File " + path + " has size " + size, LOG_ENDPOINT.FILE_SYSTEM));
+    return size;
 }
