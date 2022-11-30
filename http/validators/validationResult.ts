f06d161d0d5e67aeb6259fe91944a2f1294bf018
MetaEMK <jan-benecke@t-online.de>
2022-11-30 20:14:36 +0100

    updating validationResult
    changes constuctor and basic implementation

--- http/validators/validationResult.ts
+++ http/validators/validationResult.ts
@@ -1,13 +1,9 @@
-class ValidationResult {
+import { ValidationError } from './validationError.js';
+export class ValidationResult {
     public errors?: ValidationError[];
-    public validatedData: object;
+    public validatedData?: object;
 
     public isValid = () => {
         return !this.errors;
-    }
-
-    constructor(validatedData: object, errors?: ValidationError[]) {
-        this.validatedData = validatedData;
-        this.errors = errors;
     }
 }
