ef76b5cae75b5e9a28bc7912a5ad5a3b53abe952
MetaEMK <Jan-Benecke@t-online.de>
2022-12-01 16:07:40 +0100

    adding conversion_type.validator
    added ValidName

---
+++ http/validators/conversion_type.validator.ts
@@ -0,0 +1,20 @@
+import { Validator } from "./MainValidator";
+import { ValidatorIdUtilities } from "./util/validatorIdUtilities";
+
+
+export class ConversionTypeValidator extends Validator
+{
+    public isValidName(name?: any): boolean
+    {
+        let val = new ValidatorIdUtilities();
+        if(!val.isValidId("ConversionTypeValidator" ,name))
+        {
+            this.logError("Invalid name", name);
+            return false;
+        }
+
+        this.logSuccess("ConversionTypeValidator", "name is valid", name);
+        return true;
+    }
+
+}
