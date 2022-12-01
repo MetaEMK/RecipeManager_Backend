1befd675a3a5a846faef5cd3316d36c61c94476b
MetaEMK <Jan-Benecke@t-online.de>
2022-12-01 16:45:45 +0100

    adding more validators
    - conversion to size (complete)
    - schedule items (complete)

--- enums/GeneralValidationErrors.enum.ts
+++ enums/GeneralValidationErrors.enum.ts
@@ -12,6 +12,10 @@ export enum GeneralValidationErrorCodes {
     QUANTITY_MISSING = "Quantity is missing",
     QUANTITY_INVALID = "Quantity is invalid",
 
+    //MULTIPLICATOR
+    MULTIPLICATOR_MISSING = "Multiplicator is missing",
+    MULTIPLICATOR_INVALID = "Multiplicator is invalid",
+
     //DESCRIPTION
     DESCRIPTION_MISSING = "Description is missing",
     DESCRIPTION_INVALID = "Description must be a string",
@@ -28,6 +32,15 @@ export enum GeneralValidationErrorCodes {
     //BRANCH ID
     BRANCH_IDS_MISSING = "Branch_id array is missing",
     BRANCH_IDS_INVALID = "Branch_id array is invalid",
+    BRANCH_ID_MISSING = "Branch ID is missing",
     BRANCH_ID_INVALID = "Branch ID is invalid",
     BRANCH_ID_NOT_UNIQUE = "Branch ID is not unique",
+
+    //DAY
+    DAY_MISSING = "Day is missing",
+    DAY_INVALID = "Day is invalid, Valid Numbers: 1-7",
+
+    //VARIANT ID
+    VARIANT_ID_MISSING = "Variant ID is missing",
+    VARIANT_ID_INVALID = "Variant ID is invalid",
 }
