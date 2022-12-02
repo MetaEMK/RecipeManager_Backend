87f313f9bf58363796e8c4bd023dbcaf89cbdb40
Jonny Vang <jonny.vang@nussbaum-medien.de>
2022-12-02 14:03:27 +0100

    Utils - Renamed uri to controller and added comments

--- utils/controller.util.ts
+++ utils/controller.util.ts
@@ -1,3 +1,9 @@
+/**
+ * Decodes URI characters %20 and + to a space character.
+ * 
+ * @param uriComponent URI component to decode
+ * @returns Decoded URI component
+ */
 export function decodeURISpaces(uriComponent: string|undefined): string|undefined
 {
     if (uriComponent) {
