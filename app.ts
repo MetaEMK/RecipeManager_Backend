121e2fa0df28eb3e3a1b24ea82d184d85da0aa89
Jonny Vang <jonny.vang@nussbaum-medien.de>
2022-12-08 18:03:20 +0100

    Codebase - Removed useless boilerplate code

--- app.ts
+++ app.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import { Request, Response } from "express";
-import path from "path";
 import { apiRouter } from "./api.js";
 import dotenv from "dotenv";
 
@@ -9,11 +7,6 @@ dotenv.config({ path: "./config/app.env"});
 const app = express();
 
 app.use("/api/v1", apiRouter);
-
-app.use(express.static(process.env.FRONTEND_DIST_PATH!));
-app.use((req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, process.env.FRONTEND_DIST_PATH!, "index.html"))
-});
 
 app.listen(process.env.NODE_PORT, () => {
     console.log(`App listening at http://localhost:${process.env.NODE_PORT}`);
