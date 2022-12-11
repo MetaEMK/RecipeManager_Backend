5f152b91e10e0d522d1f45fb8d64a29ae3f91b9c
Jonny Vang <jonny-vang@web.de>
2022-12-11 23:57:14 +0100

    Fixed typing error

--- http/middleware/json.middleware.ts
+++ http/middleware/json.middleware.ts
@@ -1,4 +1,4 @@
-import {  Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { createLogger, LOG_ENDPOINT } from "../../utils/logger.js";
 
 /**
