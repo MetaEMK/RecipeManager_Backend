db67ca357efc28df6693719b73c4514baec54f3f
Jonny Vang <jonny-vang@web.de>
2022-12-12 03:58:00 +0100

    Router - Adjusted schedule items endpoint parameters

--- http/controllers/schedule.controller.ts
+++ http/controllers/schedule.controller.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AppDataSource } from "../../config/datasource.js";
-import { createLogger } from "../../utils/logger.js";
 import { decodeURISpaces, generateSlug } from "../../utils/controller.util.js";
-import { SQLiteErrorResponse } from "../error_responses/sqliteErrorResponse.js";
+import { HttpNotFoundException } from "../../exceptions/HttpException.js";
+import { createLogger, LOG_ENDPOINT } from "../../utils/logger.js";
 import { Branch } from "../../data/entities/branch.entity.js";
 import { ScheduledItem } from "../../data/entities/scheduled_item.entity";
 import { ScheduleItemsValidator } from "../validators/schedule_items.validator";
+import { ValidationException } from "../../exceptions/ValidationException.js";
 
 // Router instance
 export const scheduleRouter = express.Router({
@@ -19,20 +19,20 @@ const logger = createLogger();
 /**
  * Get a specific schedule with all its items.
  */
-scheduleRouter.get("/", async function (req: Request, res: Response) {
+scheduleRouter.get("/", async function (req: Request, res: Response, next: NextFunction) {
 
 });
 
 /**
  * Create a schedule item for a specific schedule.
  */
-scheduleRouter.post("/", async function (req: Request, res: Response) {
+scheduleRouter.post("/", async function (req: Request, res: Response, next: NextFunction) {
 
 });
 
 /**
  * Delete a schedule item of a specific schedule.
  */
-scheduleRouter.delete("/:id", async function (req: Request, res: Response) {
+scheduleRouter.delete("/:id", async function (req: Request, res: Response, next: NextFunction) {
 
 });
