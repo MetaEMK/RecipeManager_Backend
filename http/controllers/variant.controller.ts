48308ced1bc792f5c9c703d6155c710dd17add0c
Jonny Vang <jonny-vang@web.de>
2022-12-12 04:17:13 +0100

    Router - Adjusted variant endpoint parameters

--- http/controllers/variant.controller.ts
+++ http/controllers/variant.controller.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AppDataSource } from "../../config/datasource.js";
-import { createLogger } from "../../utils/logger.js";
 import { decodeURISpaces, generateSlug } from "../../utils/controller.util.js";
-import { SQLiteErrorResponse } from "../error_responses/sqliteErrorResponse.js";
+import { HttpNotFoundException } from "../../exceptions/HttpException.js";
+import { createLogger } from "../../utils/logger.js";
 import { Recipe } from "../../data/entities/recipe.entity.js";
 import { Variant } from "../../data/entities/variant.entity.js";
 import { VariantIngredient } from "../../data/entities/variant_ingredient.entity.js";
 import { VariantValidator } from "../validators/variant.validator.js";
 import { VariantIngredientValidator } from "../validators/variantIngredient.validator.js";
+import { ValidationException } from "../../exceptions/ValidationException.js";
 
 // Router instance
 export const variantRouter = express.Router({
@@ -21,34 +21,34 @@ const logger = createLogger();
 /**
  * Get all recipe variants.
  */
-variantRouter.get("/", async function (req: Request, res: Response) {
+variantRouter.get("/", async function (req: Request, res: Response, next: NextFunction) {
 
 });
 
 /**
  * Get a specific recipe variant.
  */
- variantRouter.get("/:id", async function (req: Request, res: Response) {
+ variantRouter.get("/:id", async function (req: Request, res: Response, next: NextFunction) {
 
 });
 
 /**
  * Create a recipe variant.
  */
-variantRouter.post("/", async function (req: Request, res: Response) {
+variantRouter.post("/", async function (req: Request, res: Response, next: NextFunction) {
 
 });
 
 /**
  * (Partially) Update a recipe variant.
  */
-variantRouter.patch("/:id", async function (req: Request, res: Response) {
+variantRouter.patch("/:id", async function (req: Request, res: Response, next: NextFunction) {
 
 });
 
 /**
  * Delete a recipe variant.
  */
-variantRouter.delete("/:id", async function (req: Request, res: Response) {
+variantRouter.delete("/:id", async function (req: Request, res: Response, next: NextFunction) {
 
 });
